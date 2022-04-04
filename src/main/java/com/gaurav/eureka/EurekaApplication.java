f93509a6e369e9cdfb8b48202841bd0eeb62d920
Gaurav Thakur <gauravtha2406@gmail.com>
2022-04-05 00:18:00 +0530

    eureka-server added

---
+++ src/main/java/com/gaurav/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.gaurav.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author Ashish
+ * 
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
